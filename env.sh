a8f0fcc9b8ff1910ec20a7708c1fa9e61c9a29d5
Lucas <llanga@student.wethinkcode.co.za>
2019-05-25 12:16:10 +0200

    added note for getiing other files

--- env.sh
+++ env.sh
@@ -3,9 +3,11 @@ sh -c "$(curl -fsSL https://raw.githubusercontent.com/Tolsadus/42homebrewfix/mas
 curl -O https://fastdl.mongodb.org/osx/mongodb-osx-ssl-x86_64-4.0.9.tgz
 mv mongodb-osx-ssl-x86_64-4.0.9.tgz ~/Desktop/
 open ~/Desktop/mongodb-osx-ssl-x86_64-4.0.9.tgz
-#mortalscumbag
-#crcandy
-#geoffgarside
-#gentoo-y
-#mh
-#xiong-chiamiov-plus - y josh
+#ohmyzsh themes
+    #mortalscumbag
+    #crcandy
+    #geoffgarside
+    #gentoo-y
+    #mh
+    #xiong-chiamiov-plus - y josh
+    #download individual files,add
