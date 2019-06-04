4662ca100e4d67716ec3ae249b4b5ef4bc51c7eb
Lucas <llanga@student.wethinkcode.co.za>
2019-06-04 13:08:39 +0200

    Update env.sh

--- env.sh
+++ env.sh
@@ -2,7 +2,7 @@ sh -c "$(curl -fsSL https://raw.githubusercontent.com/Tolsadus/42homebrewfix/mas
 #curl -O https://fastdl.mongodb.org/osx/mongodb-osx-ssl-x86_64-4.0.9.tgz
 #mv mongodb-osx-ssl-x86_64-4.0.9.tgz ~/Desktop/
 #open ~/Desktop/mongodb-osx-ssl-x86_64-4.0.9.tgz
-sh -c "$(curl -fsSL https://github.com/powerline/fonts.git --depth=1)"
+sh -c "$(curl -fsSL https://github.com/powerline/fonts)"
 ./fonts/install.sh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 #ohmyzsh themes
